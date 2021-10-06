6f142ab1b63cf5239c05502c2bf2e3ede6011f3c
Rafa311018 <rafael311018@gmail.com>
2021-10-06 16:53:58 -0500

    onTextChanged

--- app/src/main/java/com/example/manifesto/mainscreen/MainScreenFragment.kt
+++ app/src/main/java/com/example/manifesto/mainscreen/MainScreenFragment.kt
@@ -5,15 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import com.example.manifesto.R
+import com.example.manifesto.databinding.FragmentMainBinding
 
 class MainScreenFragment : Fragment() {
+
+    private val viewModel: MainScreenViewModel by viewModels()
+
+    private lateinit var binding: FragmentMainBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        binding = FragmentMainBinding.inflate(inflater,container, false)
+        binding.viewModel = viewModel
+        binding.viewModel
+
+        viewModel.navigateToSignIn.observe(viewLifecycleOwner, Observer { navigate ->
+            if (navigate) {
+                this.findNavController().navigate(
+                    MainScreenFragmentDirections.
+                    actionMainScreenFragmentToSignInFragment())
+                viewModel.doneNavigating()
+            }
+        })
+
+        return binding.root
     }
 }
--- app/src/main/java/com/example/manifesto/signin/SignInViewModel.kt
+++ app/src/main/java/com/example/manifesto/signin/SignInViewModel.kt
@@ -1,10 +1,31 @@
 package com.example.manifesto.signin
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class SignInViewModel: ViewModel() {
-    fun hello() {
-        Log.d("Rafael", "Hi")
+class SignInViewModel : ViewModel() {
+    val _name: MutableLiveData<String> = MutableLiveData()
+    val name: LiveData<String>
+        get() = _name
+
+    val _nameError: MutableLiveData<Boolean> = MutableLiveData()
+    val nameError: LiveData<Boolean>
+        get() = _nameError
+
+    fun updateName(text: CharSequence) {
+        _name.value = text.toString()
+        Log.d("Yoshi", "${_name.value}")
+        checkNameLength(_name.value.toString())
+    }
+
+    fun checkNameLength(name: String) {
+        if (name.length < 2 || name.length > 12) {
+            _nameError.value = true
+
+        } else {
+            _nameError.value = false
+        }
     }
 }
